44527607c2064397d3dbf972c48120c4d1f7b8d6
Jomar Morais do Nascimento <67244856+jomarmn@users.noreply.github.com>
2022-11-08 15:58:54 -0300

    Update instala-zabbix.sh
    Correção de path e sed

--- instala-zabbix.sh
+++ instala-zabbix.sh
@@ -7,14 +7,14 @@
 # Autoria       | Jomar Nascimento (jomar.nascimento@faciltecnologia.com.br)
 # Revisão       | Jomar Nascimento (jomar.nascimento@faciltecnologia.com.br)
 # Data crĩação  | 04/11/2022
-# Data revisão  | 04/10/2022
-# Versão        | 1.0
+# Data revisão  | 08/11/2022
+# Versão        | 1.1
 #===================================================================================#
 #                                      Tutorial                                     #
 #===================================================================================#
 ### TUTORIAL:
 #
-# bash install-zb6-debian11.sh
+# bash instala-zabbix.sh
 # OBS.: Para executar esse script, é necessário executar como root 
 
 #===================================================================================#
@@ -37,6 +37,9 @@ function erro() {
 #===================================================================================#
 sou_root || erro "É necessário executar como root!"
 inicia_duracao
+
+# Correção de PATH
+export PATH="/sbin:/usr/sbin:/bin:/usr/bin:/usr/local/bin:/usr/local/sbin:/usr/games"
 
 # Instala o repositório do Zabbix
 wget https://repo.zabbix.com/zabbix/6.0/debian/pool/main/z/zabbix-release/zabbix-release_6.0-4%2Bdebian11_all.deb
@@ -61,11 +64,11 @@ sudo -u postgres createdb -O zabbix zabbix
 # Importa o esquema inicial e dados
 zcat /usr/share/zabbix-sql-scripts/postgresql/server.sql.gz | sudo -u zabbix psql zabbix
 
-# Configuração da senha
-sed -i.BKP "s/# DBPassword=/DBPassword=zabbix123./g" /etc/zabbix/zabbix_server.conf
+# Configuração da senha do zabbix
+sed -i.BKP "s/\#\ DBPassword=/DBPassword=zabbix123./g" /etc/zabbix/zabbix_server.conf
 
 # Configuração de local do apache
-sed -i.BKP "s/# php_value date.timezone Europe\/Riga/php_value date.timezone America\/Recife/g" /etc/apache2/conf-available/zabbix.conf
+sed -i.BKP "s/\#\ php_value\ date.timezone\ Europe\/Riga/php_value date.timezone America\/Recife/g" /etc/apache2/conf-available/zabbix.conf
 
 # Reinicia o servidor Zabbix e os processos agentes e adiciona à inicialização do sistema
 systemctl restart zabbix-server zabbix-agent apache2
